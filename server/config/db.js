51de1c0469ad5dbd2a607eec17b97149d120e50b
Kjumar <jganguli@hotmail.ca>
2020-10-26 23:46:44 -0400

    connected to MongoDB server

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+
+  //online MongoDB
+  "URI": "mongodb+srv://jayganguli:YXEtv2w32kYD4Ihz@mongodbcluster.k566r.mongodb.net/midterm?retryWrites=true&w=majority"
 };
