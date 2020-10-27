1bc181e90a274d46b81a030d26d9869c103c9d11
Kjumar <jganguli@hotmail.ca>
2020-10-27 01:43:36 -0400

    fixed app.js delete confirmation popup pointing to a non-existent page

--- client/Scripts/app.js
+++ client/Scripts/app.js
@@ -108,7 +108,7 @@ $.fn.pageMe = function(opts){
   $(".btn-danger").click(function(event){
     if(!confirm("Are you sure?")) {
       event.preventDefault();
-      window.location.assign("/games");
+      window.location.assign("/books"); // the return link used to be '/games', which doesn't exist on this site...
     }
   });
 
